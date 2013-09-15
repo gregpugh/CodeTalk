acee4968d969e34467f6ef3190f8096ae0a52cba
Greg Pugh <gpugh@nc.rr.com>
2013-09-15 18:06:52 -0400

    initial commit - really

---
+++ CodeTalk.DataSource/Repositories/TalkRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeTalk.Domain.Contracts;
+using CodeTalk.Domain.Contracts.Repositories;
+
+namespace CodeTalk.DataSource.Repositories
+{
+    public class TalkRepository: ITalkRepository
+    {
+        public IQueryable<Domain.Models.Talk> GetTalks()
+        {
+            var ctx = new CodeTalkContext();
+            return ctx.Talks;
+        }
+
+        public bool AddTalk(Domain.Models.Talk newTalk)
+        {
+            var ctx = new CodeTalkContext();
+            ctx.Talks.Add(newTalk);
+            ctx.SaveChanges();
+            return true;
+        }
+    }
+}
